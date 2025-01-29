a18b0db2dccf1b89209dc244f4d3b2e0796b797a
abhinavvermaa <abhinavvermax@gmail.com>
2025-01-29 12:49:36 +0530

    added the json file for fetching the project data

--- JS-pro/05_iterations/seven.js
+++ JS-pro/05_iterations/seven.js
@@ -9,3 +9,29 @@ const newNums = mynumbers
                 .map((num)=>num+1)
                 .filter((num)=>num>=40)
 console.log(newNums)
+const projectsData = [
+  {
+    title: "Seamless Shopping Website",
+    description:
+      "Developed an e-commerce platform with functionalities like login/signup and add-to-cart. Designed for intuitive navigation and user experience.",
+    technologies: ["React", "Bootstrap CSS", "Node", "React-Router"]
+  },
+  {
+    title: "Employee Management System (EMS)",
+    description:
+      "Developed an Employee Management System using React.js and Tailwind CSS.",
+    technologies: ["React", "Tailwind CSS", "Context API"]
+  },
+  {
+    title: "Currency Converter App",
+    description:
+      "Created a dynamic app utilizing a currency API and custom React hooks, featuring an intuitive UI for real-time currency conversion.",
+    technologies: ["React", "Tailwind CSS", "Context API"]
+  }
+];
+
+
+  const s = projectsData.map((projects, i) => {
+    return projects.description;
+  });
+  console.log(s);
